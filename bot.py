aa85a0ead3c3f48fc1e3cb2a6811de2d3f361397
diiogotk <31125778+diiogotk@users.noreply.github.com>
2019-09-20 01:34:01 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -147,4 +147,6 @@ def results():
 def webhook():
     # return response
     return make_response(jsonify(results()))
-
+if __name__ == '__main__':  
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
